d275a208ca945f4631e09a22fffb0248ff4f1236
sss3287 <sss3287@rit.edu>
2022-09-30 22:17:18 +0400

    Added the properties of the product in the Product Class (Model)

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
@@ -5,5 +5,14 @@ import java.util.logging.Logger;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Product {
-
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("price")
+    private double price;
+    @JsonProperty("quantity")
+    private int quantity;
+    @JsonProperty("calories")
+    private int calories;
 }
