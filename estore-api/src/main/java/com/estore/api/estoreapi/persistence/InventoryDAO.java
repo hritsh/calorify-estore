ab75a0350997858a516fe5afe2e5abed16403dcd
sss3287 <sss3287@rit.edu>
2022-10-01 12:25:38 +0400

    Added Delete Product to the InventoryDAO Interface

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
@@ -4,5 +4,16 @@ import java.io.IOException;
 import com.estore.api.estoreapi.model.Product;
 
 public interface InventoryDAO {
-
+    /**
+     * Deletes a {@linkplain Product product} with the given id
+     * 
+     * @param id The id of the {@link Product product}
+     * 
+     * @return true if the {@link Product product} was deleted
+     * <br>
+     * false if product with the given id does not exist
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     */
+    boolean deleteProduct(int id) throws IOException;
 }
