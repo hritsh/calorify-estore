0aa00827f2e73d2623537022b7e9d3bb0c75f3a3
Christin <caa8062@rit.edu>
2022-10-01 12:41:01 +0400

    Added searchProduct method to InventoryDAO

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
@@ -4,5 +4,14 @@ import java.io.IOException;
 import com.estore.api.estoreapi.model.Product;
 
 public interface InventoryDAO {
-
+    /**
+     * Finds all {@linkplain Product products} whose name contains the given text
+     * 
+     * @param containsText The text to match against
+     * 
+     * @return An array of {@link Product product} whose names contains the given text, may be empty
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+    Product[] searchProduct(String containsText) throws IOException;
 }
