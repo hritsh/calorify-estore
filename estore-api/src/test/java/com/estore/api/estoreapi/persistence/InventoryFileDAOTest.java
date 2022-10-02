26da80a2878de12ef5e8692d6e29f1720413bd80
sss3287 <sss3287@rit.edu>
2022-10-02 19:04:11 +0400

    Test passed for get product in InventoryFileDAOTest

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
@@ -27,5 +27,47 @@ import org.junit.jupiter.api.Test;
  */
 @Tag("Persistence-tier")
 public class InventoryFileDAOTest {
-    
+    InventoryFileDAO inventoryFileDAO;
+    Product[] testInventory;
+    ObjectMapper mockObjectMapper;
+
+    /**
+     * Before each test, we will create and inject a Mock Object Mapper to
+     * isolate the tests from the underlying file
+     * @throws IOException
+     */
+    @BeforeEach
+    public void setupInventoryFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testInventory = new Product[3];
+        testInventory[0] = new Product(1, "Apple", "", 100, 5);
+        testInventory[1] = new Product(2,"Pomogrenate", "", 50, 8);
+        testInventory[2] = new Product(3,"Galactic Potato","", 200, 10);
+
+        // When the object mapper is supposed to read from the file
+        // the mock object mapper will return the product array above
+        when(mockObjectMapper
+            .readValue(new File("doesnt_matter.txt"),Product[].class))
+                .thenReturn(testInventory);
+        inventoryFileDAO = new InventoryFileDAO("doesnt_matter.txt",mockObjectMapper);
+    }
+    @Test
+    public void testGetProduct() throws IOException {
+        // Invoke
+        Product product = inventoryFileDAO.getProduct(2);
+
+        // Analzye
+        assertEquals(product,testInventory[1]);
+    }
+
+  
+    @Test
+    public void testGetProductNotFound() throws IOException {
+        // Invoke
+        Product product = inventoryFileDAO.getProduct(98);
+
+        // Analyze
+        assertEquals(product,null);
+    }
 }
+
