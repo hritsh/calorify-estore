746ca1a11d6f6f87a1308d6209b899605b277580
Bbosss3 <114011986+Bbosss3@users.noreply.github.com>
2022-10-02 14:10:15 +0400

    Updated Inventory DAO interface

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
@@ -4,5 +4,12 @@ import java.io.IOException;
 import com.estore.api.estoreapi.model.Product;
 
 public interface InventoryDAO {
-
+  /**
+     * Retrieves all {@linkplain Product product}
+     * 
+     * @return An array of {@link Product product} objects, may be empty
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+  Hero[] getHeroes() throws IOException;
 }
