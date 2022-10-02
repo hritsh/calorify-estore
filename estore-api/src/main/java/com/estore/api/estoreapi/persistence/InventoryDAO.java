4a7dbcc2404500ff2a736afdbd88d3134f9423c3
sss3287 <sss3287@rit.edu>
2022-10-02 18:37:06 +0400

    Added Get Product to the InventoryDAO Interface

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
@@ -4,6 +4,21 @@ import java.io.IOException;
 import com.estore.api.estoreapi.model.Product;
 
 public interface InventoryDAO {
+
+/**
+     * Retrieves a {@linkplain Product product} with the given id
+     * 
+     * @param id The id of the {@link Product product} to get
+     * 
+     * @return a {@link Product product} object with the matching id
+     * <br>
+     * null if no {@link Product product} with a matching id is found
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+    Product getProduct(int id) throws IOException;
+
+
     /**
      * Deletes a {@linkplain Product product} with the given id of a particular product.
      * 
