3aa3fbd04f2913391a859875d5a363689e46a2b6
Christin <caa8062@rit.edu>
2022-10-02 17:01:50 +0400

    Added tests for searchProduct functionality in InventoryFileDAOTest.java

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
@@ -1,5 +1,100 @@
 package com.estore.api.estoreapi.persistence;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.estore.api.estoreapi.model.Product;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the Inventory File DAO class
+ * 
+ * @author 
+ */
+@Tag("Persistence-tier")
 public class InventoryFileDAOTest {
-    
+    InventoryFileDAO inventoryFileDAO;
+    Product[] testProducts;
+    ObjectMapper mockObjectMapper;
+
+    /**
+     * Before each test, we will create and inject a Mock Object Mapper to
+     * isolate the tests from the underlying file
+     * @throws IOException
+     */
+    @BeforeEach
+    public void setupInventoryFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testProducts = new Product[3];
+        testProducts[0] = new Product(1, "Apple", "", 100, 5);
+        testProducts[1] = new Product(2,"Pomogrenate", "", 50, 8);
+        testProducts[2] = new Product(3,"Galactic Potato","", 200, 10);
+
+        // When the object mapper is supposed to read from the file
+        // the mock object mapper will return the hero array above
+        when(mockObjectMapper
+            .readValue(new File("doesnt_matter.txt"),Product[].class))
+                .thenReturn(testProducts);
+        inventoryFileDAO = new InventoryFileDAO("doesnt_matter.txt",mockObjectMapper);
+    }
+
+    @Test
+    public void testSearchProduct() throws IOException{
+        // Invoke
+        Product[] products = inventoryFileDAO.searchProduct("po", null);
+
+        // Analyze
+        assertEquals(products.length,2);
+        assertEquals(products[0],testProducts[1]);
+        assertEquals(products[1],testProducts[2]);
+    }
+
+    /* Include when createProducts has been implemented
+    @Test
+    public void testSaveException() throws IOException{
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .writeValue(any(File.class),any(Product[].class));
+
+        Product product = new Product(4,"Bombasto Pepper","",300,20);
+
+        assertThrows(IOException.class,
+                        () -> inventoryFileDAO.createProduct(product),
+                        "IOException not thrown");
+    }
+    */
+
+    @Test
+    public void testConstructorException() throws IOException {
+        // Setup
+        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
+        // We want to simulate with a Mock Object Mapper that an
+        // exception was raised during JSON object deseerialization
+        // into Java objects
+        // When the Mock Object Mapper readValue method is called
+        // from the inventoryFileDAO load method, an IOException is
+        // raised
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .readValue(new File("doesnt_matter.txt"),Product[].class);
+
+        // Invoke & Analyze
+        assertThrows(IOException.class,
+                        () -> new InventoryFileDAO("doesnt_matter.txt",mockObjectMapper),
+                        "IOException not thrown");
+    }
 }
