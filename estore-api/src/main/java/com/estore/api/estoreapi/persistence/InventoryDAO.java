2b6b94880d96de03292ea6b395e36932441960fb
hritsh <hritish.mahajan@gmail.com>
2022-10-03 12:42:34 +0400

    Add CreateProduct method to InventoryDAO interface

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
@@ -3,6 +3,23 @@ package com.estore.api.estoreapi.persistence;
 import java.io.IOException;
 import com.estore.api.estoreapi.model.Product;
 
+/**
+ * Defines the interface for Product object persistence
+ * 
+ * @author SWEN Faculty
+ */
 public interface InventoryDAO {
-
+    /**
+     * Creates and saves a {@linkplain Product product}
+     * 
+     * @param product {@linkplain Product product} object to be created and saved
+     *                <br>
+     *                The id of the product object is ignored and a new uniqe id is
+     *                assigned
+     *
+     * @return new {@link Product product} if successful, false otherwise
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
+    Product createProduct(Product product) throws IOException;
 }
