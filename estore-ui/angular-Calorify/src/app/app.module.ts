ead64cd2980d2e1babccd16b0a26a1da6eed3a63
Christin <caa8062@rit.edu>
2022-10-18 13:32:28 +0400

    Create components in angular for the pages to be displayed

--- estore-ui/angular-Calorify/src/app/app.module.ts
+++ estore-ui/angular-Calorify/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventoryPageComponent } from './inventory-page/inventory-page.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CustomerCheckoutComponent } from './customer-checkout/customer-checkout.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryPageComponent,
+    ProductDetailsComponent,
+    RegistrationPageComponent,
+    LoginPageComponent,
+    ProductListingComponent,
+    ShoppingCartComponent,
+    CustomerCheckoutComponent,
+    ProductSearchComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
