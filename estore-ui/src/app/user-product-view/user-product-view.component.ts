5023cfcacec7e121a1468268e72a305b4c6a249f
Boss <basithussainsyed@gmail.com>
2022-10-28 12:45:41 +0400

    Generated the components

---
+++ estore-ui/src/app/user-product-view/user-product-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-product-view',
+  templateUrl: './user-product-view.component.html',
+  styleUrls: ['./user-product-view.component.css']
+})
+export class UserProductViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
