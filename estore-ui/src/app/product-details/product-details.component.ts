6c0c649cb633d60db5790722ca9bb65925e6eaeb
sss3287 <sss3287@rit.edu>
2022-10-31 09:08:03 +0400

    Added base functionality to the product-details

---
+++ estore-ui/src/app/product-details/product-details.component.ts
@@ -0,0 +1,21 @@
+/**
+ * SWEN 261
+ * product-details.component.ts
+ *
+ * The component that shows the details of a {@link Product product} to the admin
+ * This is where an admin is able to change the details of said {@link Product product}
+ *
+ * Contributors: Team-E
+ */
+
+ import { Component, Input, OnInit } from '@angular/core';
+ import { ProductService } from '../product.service';
+ import { Product } from '../product';
+ import { ActivatedRoute, Router } from '@angular/router';
+ import { Location } from '@angular/common';
+
+ @Component({
+   selector: 'app-product-details',
+   templateUrl: './product-details.component.html',
+   styleUrls: ['./product-details.component.css']
+ })
