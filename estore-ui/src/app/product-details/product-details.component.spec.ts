1c0c3b1979c71c00b20ec58edc6154bf763c7840
sss3287 <sss3287@rit.edu>
2022-10-31 09:21:43 +0400

    Base functionality for spec

---
+++ estore-ui/src/app/product-details/product-details.component.spec.ts
@@ -0,0 +1,14 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductDetailsComponent } from './product-details.component';
+
+describe('ProductDetailsComponent', () => {
+  let component: ProductDetailsComponent;
+  let fixture: ComponentFixture<ProductDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductDetailsComponent ]
+    })
+    .compileComponents();
+  });
