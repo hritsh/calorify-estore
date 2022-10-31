5e8b06c16b24997f1fa6471da5153af3992e6f01
sss3287 <sss3287@rit.edu>
2022-10-31 09:36:21 +0400

    spects product service done

--- estore-ui/src/app/product.service.spec.ts
+++ estore-ui/src/app/product.service.spec.ts
@@ -5,3 +5,13 @@ import { ProductService } from './product.service';
 describe('ProductService', () => {
   let service: ProductService;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ProductService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+});
