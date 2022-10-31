56fa021f2f0e93dbcef6bd5228156e1233bcf794
sss3287 <sss3287@rit.edu>
2022-10-31 09:37:49 +0400

    added injectable

--- estore-ui/src/app/product.service.ts
+++ estore-ui/src/app/product.service.ts
@@ -13,3 +13,23 @@ import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+export class ProductService {
+  private productsURL = 'http://localhost:8080/products';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Retrieves the inventory from the backend
+   *
+   * @returns a list of {@linkplain Product products} that represents the inventory
+   * of this store
+   */
+  getProducts(): Observable<Product[]> {
+    const url = `${this.productsURL}`
+    return this.http.get<Product[]>(url, this.httpOptions);
+  }
+
