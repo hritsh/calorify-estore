fa98fd95a5139b5dc4c747733a41239b590913b4
Christin <caa8062@rit.edu>
2022-11-13 16:09:29 +0400

    Added form validation and bootstrap styles to login and register

--- estore-ui/src/app/registration-page/registration-page.component.ts
+++ estore-ui/src/app/registration-page/registration-page.component.ts
@@ -3,56 +3,50 @@ import { User } from '../user';
 import { UserService } from '../user.service';
 import { Role } from '../role';
 import { RoleService } from '../role.service';
-import { map } from 'rxjs';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, FormArray, Validators, Form, FormControl, AbstractControl } from "@angular/forms";
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
   styleUrls: ['./registration-page.component.css']
 })
 export class RegistrationPageComponent implements OnInit {
-  roles: Role[] = [];
-  roleMap = new Map();
-  roleNo: number = 1;
   createdUsername: string = '';
   roleParam: Set<Role> = new Set;
-  constructor(private userService: UserService, private roleService: RoleService ) { }
+  sentUser!:User;
+  userForm!:FormGroup;
 
+  constructor(public formBuilder: FormBuilder, private userService: UserService, private roleService: RoleService, public router: Router) { }
   ngOnInit(): void {
-    this.roleService.getRoles().subscribe(response => {
-      this.roles = response;
+    this.userForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(40), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}$')]]
     })
   }
-  getCheckBoxValue(event: any) {
-    if(event.target?.checked){
-      if(event.target?.name == "user")
-        this.roleNo=2;
-      else if(event.target?.name == "admin")
-        this.roleNo=1;
-      else if(event.target?.name == "customer")
-        this.roleNo = 3;
-      this.roleMap.set(event.target?.name, this.roleNo)
-    } else {
-      this.roleMap.delete(event.target?.name);
-    }
-    console.log(this.roleMap);
+  get f(): { [key: string]: AbstractControl } {
+    return this.userForm.controls;
   }
   submitCreateUserRequest(username: string, password:string): void {
-    for(let [key,value] of this.roleMap.entries()) {
-      this.roleParam.add(new Role(value,key));
-      //this.roles.push(new Role(value, key));
-    }
+    const newRole = new Role(2,"user");
+    this.roleParam.add(newRole);
     const sentUser = new User(username, password, this.roleParam);
     console.log(this.roleParam);
-    //check code here
     this.userService.createUser(sentUser).subscribe(user => {
       this.createdUsername = user.username;
+      this.router.navigateByUrl("/login")
     });
   }
-  submit(username: string, password: string): void {
+  submit():void {
+    if(this.userForm.valid) {
+      const credentials = this.userForm.getRawValue();
+      const username = credentials.username;
+      const password = credentials.password;
+      if(username && password) {
+        this.submitCreateUserRequest(username, password)
+      }
+    } else {
+      alert("invalid form");
+    }
     this.roleParam.clear();
-    username = username.trim();
-    password = password.trim();
-    this.submitCreateUserRequest(username, password)
-
   }
 }
