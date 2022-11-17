d8d59072e245eb02d9eda459eea87e9664de7d81
sss3287 <sss3287@rit.edu>
2022-11-17 19:14:14 +0400

    appbmi component ts

---
+++ estore-ui/src/app/appbmi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './appbmi.component.html',
+  styleUrls: ['./appbmi.component.scss']
+})
+export class AppComponent {
+  title = 'BMICalculatorAppAngular';
+}
