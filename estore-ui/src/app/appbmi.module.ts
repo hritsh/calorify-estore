53223c480adce06cc99912bf8de400e6b069d4cf
sss3287 <sss3287@rit.edu>
2022-11-17 19:12:51 +0400

    appbmi module TS file names updated

--- estore-ui/src/app/appbmi.module.ts
+++ estore-ui/src/app/appbmi.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './appbmi-routing.module';
+import { AppComponent } from './appbmi.component';
 import { HomeComponent } from './components/home/home.component';
 import { ResultComponent } from './components/result/result.component';
 
