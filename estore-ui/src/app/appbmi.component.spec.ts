4575fd7fe995facb8b7303de803a97250a1820a7
sss3287 <sss3287@rit.edu>
2022-11-17 19:17:07 +0400

    appbmi component spec ts completed

--- estore-ui/src/app/appbmi.component.spec.ts
+++ estore-ui/src/app/appbmi.component.spec.ts
@@ -18,3 +18,16 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+  it(`should have as title 'BMICalculatorAppAngular'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('BMICalculatorAppAngular');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('BMICalculatorAppAngular app is running!');
+  });
+});
