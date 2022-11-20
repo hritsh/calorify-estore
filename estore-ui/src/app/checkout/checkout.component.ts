af92f630930e069020b7660e8ffbfd88ebde80a7
Eslam Tarrum <et9540@rit.edu>
2022-11-20 18:40:07 +0400

    added checkout functionality to ts

--- estore-ui/src/app/checkout/checkout.component.ts
+++ estore-ui/src/app/checkout/checkout.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-checkout',
@@ -7,11 +13,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  @Input() username!: string;
+  cart: Product[] = [];
+  @Input() user?: User;
+  ifSucceed: Boolean = false;
+  ifFailed: Boolean = false;
+
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private userService: UserService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.ifSucceed = false;
+    this.ifFailed = false;
+    this.getUser();
+    this.getCart();
+    this.getTotalCalories();
+    this.getTotalPrice();
   }
 
-  
+  /**
+   * Gets and saves the shopping cart of the currently logged in {@linkplain User user}
+   */
+   getCart(): void {
+    /**
+     * Gets the id from the route to get the cart
+     */
+    this.shoppingCartService.getCart(this.username)
+      .subscribe(cart => this.cart = cart);
+  }
+
+  /**
+   * Gets the information of the currently logged in {@linkplain User user}
+   */
+  getUser(): void {
+    this.username = localStorage.getItem('sub')!;
+    this.userService.getUser(this.username)
+      .subscribe(user => this.user = user);
+  }
+
+  getTotalCalories(): number {
+    let totalCalories = 0;
+    for (let i = 0; i < this.cart.length; i++) {
+      totalCalories += this.cart[i].calories * this.cart[i].quantity;
+    }
+    return totalCalories;
+  }
+
+  getTotalPrice(): number {
+    let totalPrice = 0;
+    for (let i = 0; i < this.cart.length; i++) {
+      totalPrice += this.cart[i].price * this.cart[i].quantity;
+    }
+    return totalPrice;
+  }
+
+  /**
+   * Returns the {@linkplain User user} to their previous page
+   */
+   backButton(): void {
+    this.location.back();
+  }
+
+  /**
+   * Initiates the action of checking out for the {@linkplain User user}
+   */
+  checkout(): void {
+    this.shoppingCartService.checkout(this.username).subscribe(output => {
+      if (output) {
+        this.ifSucceed = true;
+        this.cart = [];
+      } else { this.ifFailed = true; }
+    });
+    console.log(this.ifSucceed, this.ifFailed, this.cart);
+  }
 
 }
